07334a9d390f1a53e684ca9e4f60687d1365370c
Shvet Ghare <shvetgharework@gmail.com>
2026-07-15 12:06:19 +0530

    Time: 0 ms (100%), Space: 42.8 MB (6.12%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,29 +1,7 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd = 0;
-        int sumEven = 0;
-        int limit = n*2;
-
-        for (int i = 1; i <= limit; i++) {
-            if (i % 2 == 0) {
-                sumEven += i; // Correct: Adds 'i' to the current sum
-            } else {
-                sumOdd += i; // Correct: Adds 'i' to the current sum
-            }
-        }
-        return findGCD(sumOdd, sumEven);
-        // return sumOdd;
-        // return sumEven;
-    }
-
-    public static int findGCD(int a, int b) {
-        a = Math.abs(a);
-        b = Math.abs(b);
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+        int odd = n*n;
+        int even = n*(n+1);
+        return n;
     }
 }

9713ac27cdb786a7c65a916604d185901b06ec5f
Shvet Ghare <shvetgharework@gmail.com>
2026-08-09 11:59:31 +0530

    Time: 1 ms (97.39%), Space: 47.7 MB (43.84%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,61 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        for(int i = 0; i < col; i++) {
+            if(matrix[0][i] == 0){
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        for(int j = 0; j < row; j++){
+            if(matrix[j][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i < row; i++){
+            for(int j = 1; j < col; j++){
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < row; i++){
+            if(matrix[i][0] == 0) {
+                for(int j = 1; j < col; j++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for(int j = 1; j < col; j++){
+            if(matrix[0][j] == 0) {
+                for(int i = 1; i < row; i++){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(firstRowHasZero){
+            for(int j = 0; j < col; j++){
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(firstColHasZero){
+            for(int i = 0; i < row; i++){
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+}
